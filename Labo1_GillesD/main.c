407dab87200d76908b5328fbd90c6a457577ce25
Gilles Durnez <gilles.durnez@student.vives.be>
2022-09-28 11:41:14 +0100

    V0.2 -- Reading input

--- Labo1_GillesD/main.c
+++ Labo1_GillesD/main.c
@@ -3,28 +3,33 @@
 //#include <stdlib.h>
 #include <sys/time.h>
 //#include <mysql.h>
+#include <stdbool.h>
 
 int main()
 {
-	// if(map_peripheral(&gpio) == -1) 
-	// {
-    //    	 	printf("Failed to map the physical GPIO registers into the virtual memory space.\n");
-    //     	return -1;
-    // }
+	if(map_peripheral(&gpio) == -1) 
+	{
+       	 	printf("Failed to map the physical GPIO registers into the virtual memory space.\n");
+        	return -1;
+    }
 
-	// Define gpio 17 as input
-	//INP_GPIO(17);
+	INP_GPIO(17);
+
 	struct tm *timeinfo ;
 	time_t rawtime ;
-	char strResponse [128] ;
+	char strResponse [128];
+	bool io_17;
 
 	while(1)
 	{
+		io_17 = GPIO_READ(17);
+		printf("Input GPIO 17 = %d\n",io_17);
+
 		rawtime = time (NULL) ;
 		timeinfo = localtime(&rawtime) ;  
 		strftime(strResponse,128,"%H:%M:%S %d-%b-%Y",timeinfo);  
 
-		printf ("%s\n", strResponse) ;
+		printf ("%s\n", strResponse);
 	}
 
 	return 0;	
