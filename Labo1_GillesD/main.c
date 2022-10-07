3962c684d9d85b73c7b74e66b39c4f2ea02a219e
Gilles Durnez <gilles.durnez@student.vives.be>
2022-10-07 14:29:30 +0100

    V0.4 -- Meerdere inputs uitlezen

--- Labo1_GillesD/main.c
+++ Labo1_GillesD/main.c
@@ -54,23 +54,29 @@ int main()
     }
 	
 	INP_GPIO(17);
-	char poort17 [2] = "17";
+	INP_GPIO(27);
+	char poort17 [3] = "17";
+	char poort18 [3] = "18";
 	bool io_17_new, io_17_old = GPIO_READ(17);
+	bool io_18_new, io_18_old = GPIO_READ(18);
 
 	while(1)
 	{
 		io_17_new = GPIO_READ(17);
+		io_18_new = GPIO_READ(18);
+
+		struct tm *timeinfo ;
+		time_t rawtime ;
+		char strResponse [128];
+		
+
+		rawtime = time (NULL) ;
+		timeinfo = localtime(&rawtime) ;  
+		strftime(strResponse,128,"%H:%M:%S %d-%b-%Y",timeinfo);
+
 		if(Input_Value_Changed(&io_17_new, &io_17_old))
 		{
-		printf("NEW VALUE GPIO 17 = %d\n",io_17_new);
-
-			struct tm *timeinfo ;
-			time_t rawtime ;
-			char strResponse [128];
-
-			rawtime = time (NULL) ;
-			timeinfo = localtime(&rawtime) ;  
-			strftime(strResponse,128,"%H:%M:%S %d-%b-%Y",timeinfo);
+			printf("NEW VALUE GPIO %s = %d\n",poort17,io_17_new);
 			
 			char queryString [128];
 			sprintf(queryString,"INSERT INTO inputChanges (time, gpio, value) VALUES('%s','%s','%d');",strResponse,poort17,io_17_new);
@@ -81,12 +87,25 @@ int main()
 				mysql_close(con);
 				exit(1);
 			}
+			sleep(1);
 		}
-		
+		if(Input_Value_Changed(&io_18_new, &io_18_old))
+		{
+			printf("NEW VALUE GPIO %s = %d\n",poort18,io_18_new);
+			
+			char queryString [128];
+			sprintf(queryString,"INSERT INTO inputChanges (time, gpio, value) VALUES('%s','%s','%d');",strResponse,poort18,io_18_new);
+
+			if (mysql_query(con, queryString))
+			{
+				fprintf(stderr, "%s\n", mysql_error(con));
+				mysql_close(con);
+				exit(1);
+			}
+			sleep(1);
+		}
 	}
-
 	return 0;	
-
 }
 
 bool Input_Value_Changed(bool *newVal, bool *oldVal)
