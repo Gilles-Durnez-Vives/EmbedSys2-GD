78f9c735d93da84acd2d7db795dd5228fa94901f
Gilles Durnez <gilles.durnez@student.vives.be>
2022-10-13 15:03:12 +0200

    U0.2 -- SQL TIMESTAMP -- LIBGPIOD expension

--- Labo1_GillesD/main.c
+++ Labo1_GillesD/main.c
@@ -1,10 +1,10 @@
-#include "PJ_RPI.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
 #include <time.h>
 #include <mysql.h>
 #include <stdbool.h>
+#include <gpiod.h>
 //Uitbreiding
 bool Input_Value_Changed(bool *newVal, bool *oldVal);
 
@@ -12,6 +12,19 @@ int main()
 {
 	
 	MYSQL *con = mysql_init(NULL);
+
+	const char *chipname = "gpiochip0";
+	struct gpiod_chip *chip;
+	struct gpiod_line *gpio_pin_17;
+	struct gpiod_line *gpio_pin_18;
+
+	chip = gpiod_chip_open_by_name(chipname);
+
+	gpio_pin_17 = gpiod_chip_get_line(chip, 17);
+	gpio_pin_18 = gpiod_chip_get_line(chip, 18);
+
+	gpiod_line_request_input(gpio_pin_17, "Labo1GD");
+	gpiod_line_request_input(gpio_pin_18, "Labo1GD");
 
 	if (con == NULL)
 	{
@@ -40,46 +53,29 @@ int main()
 		exit(1);
 	}
 
-	if (mysql_query(con, "CREATE TABLE IF NOT EXISTS inputChanges( time VARCHAR(128), gpio VARCHAR(2), value INT );"))
+	if (mysql_query(con, "CREATE TABLE IF NOT EXISTS inputChanges( time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, gpio VARCHAR(3), value INT );"))
 	{
 		fprintf(stderr, "%s\n", mysql_error(con));
 		mysql_close(con);
 		exit(1);
 	}
-
-	if(map_peripheral(&gpio) == -1) 
-	{
-		printf("Failed to map the physical GPIO registers into the virtual memory space.\n");
-		return -1;
-    }
 	
-	INP_GPIO(17);
-	INP_GPIO(27);
 	char poort17 [3] = "17";
 	char poort18 [3] = "18";
-	bool io_17_new, io_17_old = GPIO_READ(17);
-	bool io_18_new, io_18_old = GPIO_READ(18);
+	bool io_17_new, io_17_old = gpiod_line_get_value(gpio_pin_17);
+	bool io_18_new, io_18_old = gpiod_line_get_value(gpio_pin_18);
 
 	while(1)
 	{
-		io_17_new = GPIO_READ(17);
-		io_18_new = GPIO_READ(18);
-
-		struct tm *timeinfo ;
-		time_t rawtime ;
-		char strResponse [128];
-		
-
-		rawtime = time (NULL) ;
-		timeinfo = localtime(&rawtime) ;  
-		strftime(strResponse,128,"%H:%M:%S %d-%b-%Y",timeinfo);
+		io_17_new = gpiod_line_get_value(gpio_pin_17);
+		io_18_new = gpiod_line_get_value(gpio_pin_18);
 
 		if(Input_Value_Changed(&io_17_new, &io_17_old))
 		{
 			printf("NEW VALUE GPIO %s = %d\n",poort17,io_17_new);
 			
 			char queryString [128];
-			sprintf(queryString,"INSERT INTO inputChanges (time, gpio, value) VALUES('%s','%s','%d');",strResponse,poort17,io_17_new);
+			sprintf(queryString,"INSERT INTO inputChanges (gpio, value) VALUES('%s','%d');",poort17,io_17_new);
 
 			if (mysql_query(con, queryString))
 			{
@@ -94,7 +90,7 @@ int main()
 			printf("NEW VALUE GPIO %s = %d\n",poort18,io_18_new);
 			
 			char queryString [128];
-			sprintf(queryString,"INSERT INTO inputChanges (time, gpio, value) VALUES('%s','%s','%d');",strResponse,poort18,io_18_new);
+			sprintf(queryString,"INSERT INTO inputChanges (gpio, value) VALUES('%s','%d');",poort18,io_18_new);
 
 			if (mysql_query(con, queryString))
 			{
@@ -105,6 +101,9 @@ int main()
 			sleep(1);
 		}
 	}
+	gpiod_line_release(gpio_pin_17);
+	gpiod_line_release(gpio_pin_18);
+	gpiod_chip_close(chip);
 	return 0;	
 }
 
